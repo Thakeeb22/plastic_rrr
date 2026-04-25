b5de6ee315b840f1dcfc471b8e68ee61b5c69508
Muhammad Thakeeb <thakeebmuhammad@gmail.com>
2026-04-25 12:28:54 +0100

    admin ui

---
+++ admin_ui/script.js
@@ -0,0 +1,87 @@
+const { handle } = require("express/lib/application");
+
+const pendingContainer = document.querySelector(".pending");
+async function loadPending() {
+  try {
+    const res = await fetch("https://plastic-rrr.onrender.com/admin/pending");
+    const data = await res.json();
+    renderPending(data);
+  } catch (err) {
+    console.error("Error loading pending:", err);
+  }
+}
+function renderPending(data) {
+  pendingContainer.innerHTML = "";
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("pending-card");
+    card.innerHTML = `
+        <h2>Profile Code: ${item.profileCode}</h2>
+        <h3>Phone Number: ${item.phone}</h3>
+        <small>Submitted Weight: ${item.weight}kg</small>
+        <small>Date/Time: ${new Date(item.createdAt).toLocaleString()}</small>
+        <div class="btns">
+        <button class="approve" data-id="${item.id}">Approve</button>
+        <button class="reject" data-id="${item.id}">Reject</button>
+        </div>
+        `;
+    pendingContainer.appendChild(card);
+  });
+}
+loadPending();
+document.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("approve")) {
+    const id = e.target.dataset.id;
+    await handleAcion(id, "approve");
+  }
+  if (e.target.classList.contains("reject")) {
+    const id = e.target.dataset.id;
+    await handleAcion(id, "reject");
+  }
+});
+async function handleAction(id, action) {
+  try {
+    const res = await fetch(`/admin/${action}/${id}`, {
+      method: "POST",
+    });
+    const data = await res.json();
+    if (data.success) {
+      loadPending();
+    }
+  } catch (err) {
+    console.error(`${action} error:`, err);
+  }
+}
+const table = document.querySelector("table");
+async function loadHistory() {
+  try {
+    const res = await fetch("https://plastic-rrr.onrender.com/admin/history");
+    const data = await res.json();
+    renderHistory(data);
+  } catch (err) {
+    console.error("Error loading history:", err);
+  }
+}
+function renderHistory(data){
+    table.innerHTML=`
+    <tr>
+    <th>Profile Code</th>
+    <th>Contact</th>
+    <th>Weight(KG)</th>
+    <th>Time Stamp</th>
+    <th>Status</th>
+    </tr>
+    `
+    data.forEach(item =>{
+        const row = document.createElement("tr")
+        row.innerHTML=`
+        <td>${item.profileCode}</td>
+        <td>${item.phone}</td>
+        <td>${item.weight}</td>
+        <td>${new Date(item.createdAt).toLocaleString()}</td>
+        <td>${item.status}</td>
+        `
+        table.appendChild(row)
+    })
+}
+loadHistory()
